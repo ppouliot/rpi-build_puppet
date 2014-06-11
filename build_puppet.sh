05c16c39b3564f0c6899e2db35446e5860c666ac
Peter J. Pouliot <ppouliot@gmail.com>
2014-06-11 11:23:17 +0000

    initial commit

---
+++ build_puppet.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir temp && cd temp
+for pkg in facter hiera ruby-rgen
+do
+    apt-get -y build-dep $pkg
+    apt-get -b source $pkg
+done
+
+apt-get install virt-what ruby-json
+apt-get install virt-what ruby-json
+dpkg -i facter_*.deb hiera_*.deb ruby-rgen_*.deb
+apt-get -y build-dep puppet
+apt-get -b source puppet
+apt-get install augeas-lenses libaugeas0 libaugeas-ruby1.8 ruby-shadow libshadow-ruby1.8
+dpkg -i puppet-common_*.deb puppet_*.deb
