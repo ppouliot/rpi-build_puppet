efbf95210ef93082938b4d10594a304d252bdc93
root <root@ospi.(none)>
2014-06-12 02:37:28 -0400

    adding the addition of the puppetlabs repo

--- build_puppet.sh
+++ build_puppet.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+cat > /etc/apt/sources.list.d/puppetlabs.list << EOF
+deb-src http://apt.puppetlabs.com wheezy main
+deb-src http://apt.puppetlabs.com wheezy dependencies
+EOF
+
+apt-get update -y
 
 mkdir temp && cd temp
 for pkg in facter hiera ruby-rgen
@@ -12,5 +18,5 @@ apt-get install virt-what ruby-json
 dpkg -i facter_*.deb hiera_*.deb ruby-rgen_*.deb
 apt-get -y build-dep puppet
 apt-get -b source puppet
-apt-get install augeas-lenses libaugeas0 libaugeas-ruby1.8 ruby-shadow libshadow-ruby1.8
+apt-get install -y augeas-lenses libaugeas0 libaugeas-ruby1.8 ruby-shadow libshadow-ruby1.8
 dpkg -i puppet-common_*.deb puppet_*.deb
